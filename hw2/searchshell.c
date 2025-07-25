c536c5d47d97010a4683202e6c68e842d9d780fe
Jisung <jisung@cs.washington.edu>
2025-07-24 22:13:10 -0700

    hw2 part c

--- hw2/searchshell.c
+++ hw2/searchshell.c
@@ -54,6 +54,16 @@ int main(int argc, char **argv) {
   // Note that you should make sure the fomatting of your
   // searchshell output exactly matches our solution binaries
   // to get full points on this part.
+  DocTable *dt = NULL;
+  MemIndex *mi = NULL;
+  printf("Indexing '%s'\n", argv[1]);
+  if (!CrawlFileTree(argv[1], &dt, &mi)) {
+    fprintf(stderr, "directory '%s' does not exist\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  ProcessQueries(dt, mi);
+  MemIndex_Free(mi);
+  DocTable_Free(dt);
   return EXIT_SUCCESS;
 }
 
@@ -70,8 +80,57 @@ static void Usage(void) {
 }
 
 static void ProcessQueries(DocTable *dt, MemIndex *mi) {
+  while (1) {
+    printf("enter query: \n");
+    char *input = NULL;
+    if (GetNextLine(stdin, &input)) {
+      free(input);
+      printf("\nshutting down...\n");
+      return;
+    }
+
+    char *words[100];
+    int count = 0;
+    char *word = strtok(input, " ");
+    while (word && count < 100) {
+      for (char *c = word; *c; c++) {
+        *c = tolower(*c);
+      }
+      words[count++] = word;
+      word = strtok(NULL, " ");
+    }
+
+    LinkedList *results = MemIndex_Search(mi, words, count);
+    if (results) {
+      LLIterator *it = LLIterator_Allocate(results);
+      while (LLIterator_IsValid(it)) {
+        SearchResult *r;
+        LLIterator_Get(it, (LLPayload_t*)&r);
+        printf(" %s (%d)\n", DocTable_GetDocName(dt, r->doc_id), r->rank);
+        LLIterator_Next(it);
+      }
+      LLIterator_Free(it);
+      LinkedList_Free(results, free);
+    }
+    
+    free(input);
+  }
 }
 
 static int GetNextLine(FILE *f, char **ret_str) {
-  return -1;  // you may need to change this return value
+  char *next_str = NULL;
+  size_t len = 0;
+  
+  if (getline(&next_str, &len, f) == -1) {
+    free(next_str);
+    return -1;
+  }
+  
+  len = strlen(next_str);
+  if (len > 0 && next_str[len-1] == '\n') {
+    next_str[len-1] = '\0';
+  }
+  
+  *ret_str = next_str;
+  return 0;
 }
